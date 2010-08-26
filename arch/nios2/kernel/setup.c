d8c5636190c3fe2cc2d519ff06d6512cdba0a8b9
Tobias Klauser <klto@zhaw.ch>
2010-08-26 10:02:12 +0200

    [InES]: Fix boot message format

--- arch/nios2/kernel/setup.c
+++ arch/nios2/kernel/setup.c
@@ -135,7 +135,7 @@ void __init setup_arch(char **cmdline_p)
 		memcpy(command_line, default_command_line, sizeof(default_command_line));
 #endif
 
-	printk("\x0F\ruClinux/Nios II\n");
+	printk("uClinux/Nios II\n");
 
 #ifdef DEBUG
 	printk("KERNEL -> TEXT=0x%08x-0x%08x DATA=0x%08x-0x%08x "
